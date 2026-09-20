3ec83ddf38466196b2e485431429f87be86e4396
gregobis <46207045+gregobis@users.noreply.github.com>
2026-09-20 09:00:31 -0500

    Render homepage intro as formatted markdown with links and paragraph breaks

--- src/components/HomepageHero/HomepageHero.js
+++ src/components/HomepageHero/HomepageHero.js
@@ -3,6 +3,27 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+const renderFormattedIntro = (text) => {
+  if (!text || typeof text !== 'string') {
+    return null
+  }
+
+  const paragraphs = text
+    .split(/\n\s*\n/)
+    .map((paragraph) => paragraph.trim())
+    .filter(Boolean)
+
+  return paragraphs.map((paragraph, paragraphIndex) => {
+    const withLinks = paragraph.replace(/\[([^\]]+)\]\((https?:\/\/[^)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>')
+
+    const withLineBreaks = withLinks.replace(/\n/g, '<br />')
+
+    return (
+      <p key={paragraphIndex} dangerouslySetInnerHTML={{ __html: withLineBreaks }} />
+    )
+  })
+}
+
 const HomepageHero = ({ data }) => {
   return (
     <Container fluid className="py-5 blue--bg">
@@ -14,11 +35,12 @@ const HomepageHero = ({ data }) => {
         </Row>
         <Row>
           <Col sm="8" lg="6" className="mx-auto">
-            <p>{data.intro.intro}</p>
+            {renderFormattedIntro(data.intro.intro)}
           </Col>
         </Row>
       </Container>
     </Container>
   )
 }
+
 export default HomepageHero
